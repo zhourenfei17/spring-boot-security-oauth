5ab88d4b925dfcaf63c5fc8843c034e2514b6628
wangchengfei <zhourenfei17@126.com>
2020-08-22 11:20:23 +0800

    @EnableGlobalMethodSecurity说明添加

--- src/main/java/com/jackeroo/oauth/config/OAuth2MethodSecurityConfiguration.java
+++ src/main/java/com/jackeroo/oauth/config/OAuth2MethodSecurityConfiguration.java
@@ -6,6 +6,14 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
+/**
+ * 开启Spring Security方法级安全
+ *
+ * prePostEnabled： 确定 前置注解[@PreAuthorize,@PostAuthorize,..] 是否启用
+ * securedEnabled： 确定安全注解 [@Secured] 是否启用
+ * jsr250Enabled： 确定 JSR-250注解 [@RolesAllowed..]是否启用
+ *
+ */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class OAuth2MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
